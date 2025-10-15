df0bb13076f2ae52b864c70fc7a8fdcbda00bf64
Martin Vazquez <vazquezlmartin33@gmail.com>
2025-10-14 22:46:27 -0700

    loginPage and registerPage finished (maybe, idk)

---
+++ src/LoginPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Header from './components/header';
+import App from './App';
+
+function LoginPage() {
+  return (
+    <div className="app">
+      <Header variant="landing" />
+
+      <div className="loginPage__container">
+        <div className="loginPage__card">
+          <h2 className="loginPage__title">Iniciar Sesión</h2>
+          <p className="loginPage__subtitle">Ingresa tus credenciales para acceder</p>
+
+          <form className="loginPage__form">
+            
+            {/* Correo */}
+            <div className="form__group full-width">
+              <label htmlFor="loginCorreo">Correo</label>
+              <input type="email" id="loginCorreo" name="loginCorreo" />
+            </div>
+            
+            {/* Contraseña */}
+            <div className="form__group full-width">
+              <label htmlFor="loginContrasena">Contraseña</label>
+              <input type="password" id="loginContrasena" name="loginContrasena" />
+            </div>
+
+            {/* Botón de envío */}
+            <button type="submit" className="btn btn--submit full-width">
+              Iniciar Sesión
+            </button>
+
+            <p className="loginPage__forgot">¿Olvidaste tu contraseña?</p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import HomePage from './HomePage.jsx'
+import RegisterPage from './RegisterPage.jsx'
+import LoginPage from './LoginPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HomePage />
+    <App />
   </StrictMode>,
 )
