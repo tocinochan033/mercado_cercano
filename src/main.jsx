b7d088cf170ab95d450c0c128c62ed118078f42d
Martin Vazquez <vazquezlmartin33@gmail.com>
2025-11-15 19:22:18 -0800

    Update dependencies and implement routing with NotFoundPage component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import ReactDom from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App.jsx'
 import HomePage from './HomePage.jsx'
 import RegisterPageCompany from './RegisterPageCompany.jsx'
 import LoginPage from './LoginPage.jsx'
 import RegisterPageUser from './RegisterPageUser.jsx'
 import FeedUI from './FeedUI.jsx'
+import { NotFoundPage } from './NotFoundPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RegisterPageCompany/>
+    <BrowserRouter>
+      <RegisterPageUser/>
+    </BrowserRouter>
   </StrictMode>,
 )
+
